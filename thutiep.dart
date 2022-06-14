fda7c48923fdff3c10b879b8578d6885588eafbf
lequocdung83 <lequocdung83@gmail.com>
2022-06-14 14:20:35 +0700

    ok

--- thutiep.dart
+++ thutiep.dart
@@ -1,3 +1,16 @@
+class product {
+  String productName = "";
+  int productYear = 0;
+  product(String productName, int productYear) {
+    this.productName = productName;
+    this.productYear = productYear;
+  }
+  String sresult() {
+    return productName + productYear.toString();
+  }
+}
+
 void main(List<String> args) {
-  print("hello git hub");
+  product opro = new product("toyota", 2022);
+  print(opro.sresult());
 }
